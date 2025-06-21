ce29820df6d6ae19212fe02f54572e17ea4dff27
Max_Mussi <maxmussi151@gmail.com>
2025-06-21 08:17:11 -0300

    Improved generation

--- terrain.py
+++ terrain.py
@@ -20,15 +20,15 @@ class World:
 
         layeredNoise = self.getLayeredNoise(cords)
         biome = self.getBiome(layeredNoise)
-        tile = biome.generate(cords, self.data)
+        tile = biome.generate(cords, self.data, self.seed)
         self.data[cords] = tile
         return tile
 
     def getLayeredNoise(self, cords):
         y, x = cords
-        temp = noise.pnoise2(x / self.biomeScale, y / self.biomeScale, octaves=3, base=self.seed + 1)
-        hum = noise.pnoise2(x / self.biomeScale, y / self.biomeScale, octaves=3, base=self.seed + 2)
-        weird = min(noise.pnoise2(x / self.biomeScale, y / self.biomeScale, octaves=3, base=self.seed + 3),0)
+        temp = noise.pnoise2(y / self.biomeScale, x / self.biomeScale, octaves=3, base=self.seed + 1)
+        hum = noise.pnoise2(y / self.biomeScale, x / self.biomeScale, octaves=3, base=self.seed + 2)
+        weird = min(noise.pnoise2(y / self.biomeScale, x / self.biomeScale, octaves=3, base=self.seed + 3),0)
         return (temp, hum, weird)
 
     def getBiome(self, layeredNoise):
@@ -36,7 +36,7 @@ class World:
         return Forest()
 
 class Biome:
-    def __init__(self, temp, hum, weird):
+    def __init__(self, temp, hum, weird, seed):
         self.temp = temp
         self.hum = hum
         self.weird = weird
@@ -49,28 +49,32 @@ class Forest(Biome):
         self.temp = 0
         self.hum = 0.2
         self.weird = 0
-        self.GRASS = (0,255,0)
+        self.GRASS = (0,128,0)
         self.GRASSBG1 = (8,100,0)
         self.GRASSBG2 = (48,100,0)
         self.FLOWER_RED = (128,0,0)
         self.FLOWER_BLUE = (0,0,128)
         self.FLOWER_PURPLE = (128,0,128)
-    def generate(self, cords, data):
-        roll = random.randint(0,1)
-        if roll == 0:
+    def generate(self, cords, data, seed):
+        y, x = cords
+        roll = noise.pnoise2(y / 8, x / 8, octaves=3, base=seed)
+        if roll <= 0:
             grassBg = self.GRASSBG1
         else:
             grassBg = self.GRASSBG2
-        roll = random.randint(1,4)
+        roll = random.randint(1,12)
         if roll == 1:
             roll = random.randint(1,48)
-            if roll <= 24:
+            if roll <= 36:
                 flower = self.FLOWER_RED
             elif roll <= 47:
                 flower = self.FLOWER_BLUE
             else:
                 flower = self.FLOWER_PURPLE
             return Tile((("º","o"),(flower,grassBg)),True,16)
-        return Tile(((".",","),(self.GRASS,grassBg)),True,16)
+        elif roll <= 3:
+            return Tile(("*",(self.GRASS,grassBg)),True,16)
+        else:
+            return Tile(((".",","),(self.GRASS,grassBg)),True,16)
         
         
